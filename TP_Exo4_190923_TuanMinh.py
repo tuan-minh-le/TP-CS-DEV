d83da50b61ba7e7c931a074c9816294c286e5d85
Tuan <tuan-minh.le@cpe.fr>
2023-09-19 12:10:18 +0700

    exo 4 completed

---
+++ TP_Exo4_190923_TuanMinh.py
@@ -0,0 +1,21 @@
+def arraymulti(MatA,MatB) :
+    MatC = []
+    result = 0
+    for i in range (len(MatA)):
+        MatC.append([])
+        for j in range(len(MatB[i])):
+             MatC[i].append(0)
+    for i in range (len(MatA)):
+         for k in range(len(MatB[i])):
+            for j in range (len(MatA[i])) : 
+                result += MatA[i][j]*MatB[j][k]
+            MatC[i][k] = result
+            result = 0
+    return MatC
+
+def main():
+    MatA = [[1,1,1],[1,1,1],[1,1,1]]
+    MatB = [[1,1,1],[1,1,1],[1,1,1]]
+    output = arraymulti(MatA,MatB)
+    return output
+ 
---
+++ testexo4.py
@@ -0,0 +1,9 @@
+from TP_Exo4_190923_TuanMinh import arraymulti
+from TP_Exo4_190923_TuanMinh import main
+MatA = [[1,1,1],[1,1,1],[1,1,1]]
+MatB = [[1,1,1],[1,1,1],[1,1,1]]
+a = 2
+b = 3
+
+test1 = print(arraymulti(MatA, MatB))
+test2 = print(main())
