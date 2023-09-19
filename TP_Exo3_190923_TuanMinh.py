d0f98202fab0c29b2d4011ffdb0e2db418462f0c
Tuan <tuan-minh.le@cpe.fr>
2023-09-19 10:43:16 +0700

    Exo 3 completed

---
+++ TP_Exo3_190923_TuanMinh.py
@@ -0,0 +1,15 @@
+def mesImpots(income) :
+    taxes = 0
+    if income >= 160337 :
+        income - income - 160337
+        taxes = income*(45/100) + (160336 - 74546)*0.41 + (74545-26071)*0.3 + (26070-10226)*0.11
+    elif income >= 74546 and income <= 160336 :
+        income = income - 74546
+        taxes = income*(41/100) + (74545-26071)*0.3 + (26070-10226)*0.11
+    elif income >= 26071 and income <= 74545 :
+        income = income - 26071
+        taxes = income*(30/100) + (26070-10226)*0.11
+    elif income >= 10226 and income <= 26070 :
+        income = income - 10226
+        taxes = income*(11/100) 
+    return int(taxes)
---
+++ testexo3.py
@@ -0,0 +1,4 @@
+from TP_Exo3_190923_TuanMinh import mesImpots
+
+test1 = print(mesImpots(50000))
+test2 = (print(mesImpots(10000)))
